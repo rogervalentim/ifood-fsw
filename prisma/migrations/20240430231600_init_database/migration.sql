1ea32b697f125dd952967477234d7d603dfe3680
rogervalentim <valentim.roger33@gmail.com>
2024-04-30 20:44:17 -0300

    chore: add seed script

--- prisma/migrations/20240430231600_init_database/migration.sql
+++ prisma/migrations/20240430231600_init_database/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "Restautant" (
+CREATE TABLE "Restaurant" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
-    "deliveryFree" DECIMAL(10,2) NOT NULL,
-    "deliveryTimeInMinutes" INTEGER NOT NULL,
+    "deliveryFee" DECIMAL(10,2) NOT NULL,
+    "deliveryTimeMinutes" INTEGER NOT NULL,
 
-    CONSTRAINT "Restautant_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Restaurant_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -33,25 +33,25 @@ CREATE TABLE "Product" (
 );
 
 -- CreateTable
-CREATE TABLE "_CategoryToRestautant" (
+CREATE TABLE "_CategoryToRestaurant" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_CategoryToRestautant_AB_unique" ON "_CategoryToRestautant"("A", "B");
+CREATE UNIQUE INDEX "_CategoryToRestaurant_AB_unique" ON "_CategoryToRestaurant"("A", "B");
 
 -- CreateIndex
-CREATE INDEX "_CategoryToRestautant_B_index" ON "_CategoryToRestautant"("B");
+CREATE INDEX "_CategoryToRestaurant_B_index" ON "_CategoryToRestaurant"("B");
 
 -- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restautant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Product" ADD CONSTRAINT "Product_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CategoryToRestautant" ADD CONSTRAINT "_CategoryToRestautant_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_CategoryToRestaurant" ADD CONSTRAINT "_CategoryToRestaurant_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_CategoryToRestautant" ADD CONSTRAINT "_CategoryToRestautant_B_fkey" FOREIGN KEY ("B") REFERENCES "Restautant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_CategoryToRestaurant" ADD CONSTRAINT "_CategoryToRestaurant_B_fkey" FOREIGN KEY ("B") REFERENCES "Restaurant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
