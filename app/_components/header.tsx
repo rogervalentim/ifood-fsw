24d64a8991271377c5281a18a7968fd240180f37
rogervalentim <valentim.roger33@gmail.com>
2024-05-01 19:26:15 -0300

    feat: add initial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+export function Header() {
+  return (
+    <div className="flex justify-between px-5 pt-6">
+      <Image src="/logo.png" alt="logo fsw foods" height={30} width={100} />
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { Header } from "./_components/header";
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="mx-4 flex justify-center bg-red-500">hello world</h1>
-    </div>
-  );
+  return <Header />;
 }
