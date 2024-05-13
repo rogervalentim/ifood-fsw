e46320c3e6134c51480328ab534de724a37344b9
rogervalentim <valentim.roger33@gmail.com>
2024-05-13 14:49:51 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { CategoryList } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até com 30% de desconto em pizzas"
+          className="h-auto w-full object-contain"
+          height={0}
+          width={0}
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
