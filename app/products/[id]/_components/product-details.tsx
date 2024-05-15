a663939e2363de2eeb6fd2f08cfb147ceca01959
rogervalentim <valentim.roger33@gmail.com>
2024-05-15 18:03:10 -0300

    feat: add cart button

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -137,6 +137,9 @@ export function ProductDetails({
         <h3 className="px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar á sacola</Button>
+      </div>
     </div>
   );
 }
