f7ceb240b3090951de35d583a5d2e51a27c08609
rogervalentim <valentim.roger33@gmail.com>
2024-06-09 17:38:11 -0300

    feat: ensure restaurant can be favorited

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -1,42 +1,73 @@
-import { Restaurant } from "@prisma/client";
+"use client";
+
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { cn } from "../_lib/utils";
+import { toggleFavoriteRestaurant } from "../_actions/restaurant";
+import { toast } from "sonner";
 
 interface RestaurantItemProps {
+  userId?: string;
   restaurant: Restaurant;
   className?: string;
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
 }
 
-export function RestaurantItem({ restaurant, className }: RestaurantItemProps) {
+export function RestaurantItem({
+  restaurant,
+  className,
+  userId,
+  userFavoriteRestaurants,
+}: RestaurantItemProps) {
+  const isFavorite = userFavoriteRestaurants.some(
+    (fav) => fav.restaurantId === restaurant.id,
+  );
+
+  const handleFavoriteClick = async () => {
+    if (!userId) return;
+
+    try {
+      await toggleFavoriteRestaurant(userId, restaurant.id);
+      toast.success(
+        isFavorite
+          ? "Restaurante removido dos favoritos."
+          : "Restaurante favoritado.",
+      );
+    } catch (error) {
+      toast.error("Erro ao favoritar o restaurante");
+    }
+  };
   return (
-    <Link
-      className={cn("min-w-[266px] max-w-[266px]", className)}
-      href={`/restaurants/${restaurant.id}`}
-    >
+    <div className={cn("min-w-[266px] max-w-[266px]", className)}>
       <div className="w-full space-y-3">
         <div className="relative h-[136px] w-full">
-          <Image
-            src={restaurant.imageUrl}
-            fill
-            className="rounded-lg object-cover"
-            alt={restaurant.name}
-          />
+          <Link href={`/restaurants/${restaurant.id}`}>
+            <Image
+              src={restaurant.imageUrl}
+              fill
+              className="rounded-lg object-cover"
+              alt={restaurant.name}
+            />
+          </Link>
 
           <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary bg-white px-2 py-[2px]">
             <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
             <span className="text-xs font-semibold text-[#323232]">5.0</span>
           </div>
 
-          <Button
-            size="icon"
-            className="absolute right-2 top-2 h-7 w-7 bg-gray-700"
-          >
-            <HeartIcon size={16} className="fill-white" />
-          </Button>
+          {userId && (
+            <Button
+              size="icon"
+              className={`absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700 ${isFavorite && "bg-primary hover:bg-gray-700"}`}
+              onClick={handleFavoriteClick}
+            >
+              <HeartIcon size={16} className="fill-white" />
+            </Button>
+          )}
         </div>
 
         <div>
@@ -63,6 +94,6 @@ export function RestaurantItem({ restaurant, className }: RestaurantItemProps) {
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,12 +1,25 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
 import { RestaurantItem } from "./restaurant-item";
+import { authOptions } from "../_lib/auth";
 
 export async function RestaurantList() {
+  const session = await getServerSession(authOptions);
+
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
+
   return (
     <section className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
-        <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        <RestaurantItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          userId={session?.user.id}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
       ))}
     </section>
   );
