bc074455cc758c4956fdf184b9cf43f553b88179
rogervalentim <valentim.roger33@gmail.com>
2024-06-23 16:33:28 -0300

    style: add web design  to recommend products page

--- app/products/recommended/page.tsx
+++ app/products/recommended/page.tsx
@@ -19,12 +19,14 @@ export default async function RecommendedProductsPage() {
     },
   });
 
+  const search = true
+
   return (
     <>
-      <Header />
-      <div className="px-5 py-6">
+      <Header search={search} />
+      <div className="px-5 py-6 lg:px-32">
         <h2 className="mb-6 text-lg font-semibold">Pedidos Recomendados</h2>
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-2 gap-6 lg:grid-cols-6 lg:gap-5">
           {products?.map((product) => (
             <ProductItem
               key={product.id}
