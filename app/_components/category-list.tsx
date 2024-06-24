6deeacf242d4a0f0fee39c6c09778117166bb774
rogervalentim <valentim.roger33@gmail.com>
2024-06-24 14:39:49 -0300

    feat: add  overflow-x-scroll category-list

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ export async function CategoryList() {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="grid grid-cols-2 gap-3 lg:flex lg:gap-5 lg:px-32">
+    <div className="flex  gap-4 overflow-x-scroll px-5 lg:gap-5 lg:px-32 [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
